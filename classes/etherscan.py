5039ff78fd38e0272de137d8dabaa4ca111903cc
Carl Ek <carlek@m1.local>
2023-02-21 12:30:07 -0500

    some minor refactoring

--- classes/etherscan.py
+++ classes/etherscan.py
@@ -10,3 +10,10 @@ class ApiError(Exception):
         super().__init__(error_msg)
         self.status_code = status_code
         self.error_msg = error_msg
+
+class EtherscanError(Exception):
+    def __init__(self, error_msg: str, status_code: int):
+        super().__init__(error_msg)
+        self.status_code = status_code
+        self.error_msg = error_msg
+
--- cli/getblocks.py
+++ cli/getblocks.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-from pydantic import BaseModel
+from classes.etherscan import GetBlockResponse
+from classes.etherscan import EtherscanError
 import pytz
 import httpx
 import click
@@ -9,20 +10,8 @@ etherscan_api_key = "valid api key from etherscan.io"
 etherscan_url = f"https://api.etherscan.io/api?module=block&action=getblocknobytime" \
                 f"&closest=before&apikey={etherscan_api_key}"
 
-class EtherscanError(Exception):
-    def __init__(self, error_msg: str, status_code: int):
-        super().__init__(error_msg)
-        self.status_code = status_code
-        self.error_msg = error_msg
 
-
-class BlockResponse(BaseModel):
-    status: int
-    message: str
-    result: int
-
-
-def call_etherscan_getblock(url: str) -> BlockResponse:
+def call_etherscan_getblock(url: str) -> GetBlockResponse:
     with httpx.Client() as client:
         resp = client.get(url)
         resp.raise_for_status()
@@ -30,14 +19,14 @@ def call_etherscan_getblock(url: str) -> BlockResponse:
             error = f"Something went wrong: {url}"
             raise EtherscanError(status_code=404, error_msg=error)
     data = resp.json()
-    return BlockResponse(status=data['status'], message=data['message'], result=data['result'])
+    return GetBlockResponse(status=data['status'], message=data['message'], result=data['result'])
 
 def get_blocks(utc_start_date: datetime, utc_end_date: datetime) -> list[str]:
     utc_date = utc_start_date
     res: list[str] = []
     while utc_date <= utc_end_date:
         timestamp = int(utc_date.timestamp())
-        block: BlockResponse = call_etherscan_getblock(etherscan_url + f"&timestamp={timestamp}")
+        block: GetBlockResponse = call_etherscan_getblock(etherscan_url + f"&timestamp={timestamp}")
         res.append(f"{utc_date.strftime('%Y-%m-%d')} {block.result}")
         utc_date = utc_date + relativedelta(days=1)
     return res
@@ -55,6 +44,7 @@ def get_blocks_cmd(start_date: str, end_date: str, time: tuple = (23, 59, 59, 0)
     res = get_blocks(utc_start_date, utc_end_date)
     for r in res:
         click.echo(r)
+
 
 if __name__ == '__main__':
     get_blocks_cmd()
